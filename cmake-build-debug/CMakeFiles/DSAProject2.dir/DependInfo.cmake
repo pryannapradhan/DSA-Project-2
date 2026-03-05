86eda49cec3f55f5c69547757ae5cdb79ff7bff4
kkCrops <kliang2006@gmail.com>
2026-03-05 12:51:51 -0500

    Added some files

---
+++ cmake-build-debug/CMakeFiles/DSAProject2.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/klian/CLionProjects/DSA-Project-2/another_sort.cpp" "CMakeFiles/DSAProject2.dir/another_sort.cpp.obj" "gcc" "CMakeFiles/DSAProject2.dir/another_sort.cpp.obj.d"
+  "C:/Users/klian/CLionProjects/DSA-Project-2/main.cpp" "CMakeFiles/DSAProject2.dir/main.cpp.obj" "gcc" "CMakeFiles/DSAProject2.dir/main.cpp.obj.d"
+  "C:/Users/klian/CLionProjects/DSA-Project-2/merge_or_quick_sort.cpp" "CMakeFiles/DSAProject2.dir/merge_or_quick_sort.cpp.obj" "gcc" "CMakeFiles/DSAProject2.dir/merge_or_quick_sort.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/DSAProject2.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DSAProject2.dir/another_sort.cpp.obj"
+  "CMakeFiles/DSAProject2.dir/another_sort.cpp.obj.d"
+  "CMakeFiles/DSAProject2.dir/main.cpp.obj"
+  "CMakeFiles/DSAProject2.dir/main.cpp.obj.d"
+  "CMakeFiles/DSAProject2.dir/merge_or_quick_sort.cpp.obj"
+  "CMakeFiles/DSAProject2.dir/merge_or_quick_sort.cpp.obj.d"
+  "DSAProject2.exe"
+  "DSAProject2.exe.manifest"
+  "DSAProject2.pdb"
+  "libDSAProject2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DSAProject2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
