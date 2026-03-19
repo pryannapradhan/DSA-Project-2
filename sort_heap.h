64b3ff94568ecc0deb2a44d1aa3c617fccebd05c
jescandor1 <jescandor1@ufl.edu>
2026-03-19 19:34:53 -0400

    update sort_heap.h

--- sort_heap.h
+++ sort_heap.h
@@ -4,5 +4,10 @@
 
 #ifndef SORT_HEAP_H
 #define SORT_HEAP_H
+#include <vector>
+#include "Restaurant.h"
+
+void heapify(vector<Restaurant>& res, int i, int n, string type);
+void heapSort(vector<Restaurant>& res, int n, string type);
 
 #endif //SORT_HEAP_H
