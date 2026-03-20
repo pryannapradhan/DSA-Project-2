16a8c8a9022bc5b31034afba8e6bfcb7245eae0d
jescandor1 <jescandor1@ufl.edu>
2026-03-19 21:33:05 -0400

    fixed heap sort

--- sort_heap.cpp
+++ sort_heap.cpp
@@ -4,8 +4,8 @@
 #include <string>
 using namespace std;
 
-void heapify(vector<Restaurant>& res, int i, int n, string type){
-    int largest = i; // root is the largest element in a max heap
+void heapifyMin(vector<Restaurant>& res, int i, int n, string type){
+    int smallest = i; // root is the smallest element in a min heap
     
     //define parent-child relationships
     int left = 2 * i + 1;
@@ -13,42 +13,66 @@ void heapify(vector<Restaurant>& res, int i, int n, string type){
 
     //compare elements based on type of sorting
     if (type == "name") {
-        if (left <= n && res[left].title < res[largest].title) {
-            largest = left;
+        if (left <= n && res[left].title < res[smallest].title) {
+            smallest = left;
         }
-        if (right <= n && res[right].title < res[largest].title) {
-            largest = right;
+        if (right <= n && res[right].title < res[smallest].title) {
+            smallest = right;
         }
-    } else if (type == "rating") {
-        if (left <= n && res[left].rating >= res[largest].rating) {
-            largest = left;
+    } 
+    
+    else if (type == "distance") {
+        if (left <= n && res[left].distance < res[smallest].distance) {
+            smallest = left;
         }
-        if (right <= n && res[right].rating >= res[largest].rating) {
-            largest = right;
-        }
-    } else if (type == "distance") {
-        if (left <= n && res[left].distance <= res[largest].distance) {
-            largest = left;
-        }
-        if (right <= n && res[right].distance <= res[largest].distance) {
-            largest = right;
+        if (right <= n && res[right].distance < res[smallest].distance) {
+            smallest = right;
         }
     }
+    // if the smallest is not the root, swap and heapify
+    if (smallest != i) {
+        swap(res[i], res[smallest]);
+        heapifyMin(res, smallest, n, type);
+    }
+}
+
+void heapifyMax(vector<Restaurant>& res, int i, int n, string type){
+    int largest = i; // root is the largest element in a max heap
+
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (type == "rating") {
+        if (left <= n && res[left].rating > res[largest].rating) {
+            largest = left;
+        }
+        if (right <= n && res[right].rating > res[largest].rating) {
+            largest = right;
+        }
+    } 
     // if the largest is not the root, swap and heapify
-    if (largest != i) {
+    if (largest != i){
         swap(res[i], res[largest]);
-        heapify(res, largest, n, type);
+        heapifyMax(res, largest, n, type);
     }
 }
 
 void heapSort(vector<Restaurant>& res, int n, string type){
-    // builds max heap
+    // builds heap
     for (int i = n / 2; i >= 0; i--) {
-        heapify(res, i, n, type);
+        if (type == "rating") {
+            heapifyMax(res, i, n, type);
+        } else {
+            heapifyMin(res, i, n, type);
+        }
     }
     //rearranges heap
     for (int i = n; i > 0; i--) {
         swap(res[0], res[i]);
-        heapify(res, 0, i, type);
+        if (type == "rating") {
+            heapifyMax(res, i-1, n, type);
+        } else {
+            heapifyMin(res, i-1, n, type);
+        }
     }
 }
